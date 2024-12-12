b507e0dc9bb5591aa0fceb4b5a5a76475714ab47
muriplz <46825658+muriplz@users.noreply.github.com>
2024-12-12 12:18:52 +0100

    added shiny.router library

--- app.R
+++ app.R
@@ -5,12 +5,16 @@ library(dplyr)
 library(gganimate)
 library(scales)
 library(gifski)
+library(shiny.router)
 
 options(shiny.port = 6968)
 
 # UI's and server instances
 source("ui/onlines.R")
 source("server/onlines.R")
+source("ui/player_movement.R")
+source("server/player_movement.R")
+
 
 # Create UI and Server
 ui <- onlines_ui()
